e627526f7a27d7e3a6a555cbc3677700e3ea7fc2
pikammmmm <pikammmmm@users.noreply.github.com>
2026-04-25 21:33:38 +0200

    feat: create dock and hud windows

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,11 +1,17 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+mod windows_setup;
+
 fn main() {
     tracing_subscriber::fmt()
         .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
         .init();
 
     tauri::Builder::default()
+        .setup(|app| {
+            windows_setup::create_windows(app)?;
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
